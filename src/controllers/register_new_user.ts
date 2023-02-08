6150081e3292466edd8c0274eeee7163c08166a5
keszinaj <kubajaniszek123@gmail.com>
2023-02-08 14:24:03 +0100

    register works

--- src/controllers/register_new_user.ts
+++ src/controllers/register_new_user.ts
@@ -1,14 +1,37 @@
 const {validationResult } = require('express-validator');
-function register_user(data)
+const argon2 = require('argon2');
+import {pushNewUser} from '../models/repo_demo' 
+async function register_user(data)
 {
-    console.log(data.body);
+    console.log(data);
+    const psw = await argon2.hash(data.password);
+    let id = Math.floor(Math.random() * 1000000) //to TRZEBA pewnie zmeinic na cos lepszego
+    let bd = new Date(data.birthdayDate)
+    let gender = data.gender
+    let new_user = {
+        ID: id,
+        Name: data.firstName,
+        Surname: data.lastName,
+        Email: data.emailAddress,
+        Password_Hash: psw,
+        Phone_number: data.phoneNumber,
+        Birthday: bd,
+        Gender: gender,
+        Country: data.country,
+        City: data.city,
+        Street: data.street_and_num,
+        Postal_Code: data.zipcode,
+        OrderIDs: []
+    }
+    pushNewUser(new_user);
+
 
 }
 function register(req, res){
     console.log("ala")
     const errors = validationResult(req);
     if (errors.isEmpty()) {
-        register_user(req);
+        register_user(req.body);
       }
     res.status(201).json({ errors: errors.array() }); //201 abysmy nie
 }
