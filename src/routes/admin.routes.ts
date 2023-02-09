6023eeccc9aaa6c9d01d8fbfcd89db20a9d07331
Jakub Ciesluk <323892@uwr.edu.pl>
2023-02-09 15:27:21 +0100

    add admin/product/id page

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { getProductbyID } from '../models/repo_demo';
 const router = express.Router();
 
 
@@ -37,8 +38,18 @@ router.get("/products/new",(req, res) => {
 
 router.get("/products/:id",(req, res) => {
     let id:string = req.params.id;
-    //for example purpose
-    res.render('admin/show_item');
+    const productID = parseInt(id);
+    if (isNaN(productID)) {
+        res.status(400).send('Invalid product ID');
+        return;
+    }
+    const product = getProductbyID(productID);
+    if (!product) {
+        res.status(404).send('Product not found');
+        return;
+    }
+
+    res.render('admin/show_item', {product: product});
 });
 
 router.get("/products/edit/:id",(req, res) => {
